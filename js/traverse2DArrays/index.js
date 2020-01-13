38a5d4b59fed4b256c9ccdf0419af59efd309157
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2020-01-13 18:37:39 -0300

    init

---
+++ js/traverse2DArrays/index.js
@@ -0,0 +1,42 @@
+const sample = [
+    [1,0,0,0,0],
+    [0,1,0,0,0],
+    [0,0,1,0,0],
+    [0,0,0,1,0],
+    [0,0,0,0,1],
+]
+
+const up = (r, c, grid) => {
+    
+}
+
+const down = (r, c, grid) => {
+    
+}
+
+const left = (r, c, grid) => {
+    
+}
+
+const right = (r, c, grid) => {
+    
+}
+
+const hasNeighbors = (r, c, grid) => {
+    return 
+}
+
+const colMapper = rowIndex => (item, index, row) => {
+
+}
+
+const rowMapper = (row, index, grid) => {
+    
+}
+
+const printNeihborsNotNull = (grid) => {
+    array2d.map()
+}
+
+const answer = printNeihborsNotNull(sample)
+console.log({answer})
